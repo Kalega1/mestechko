32bbf52bdb3615fc1ae1a25b302a8d575e3bf445
Kalega1 <kalegin.m@yandex.ru>
2023-02-10 13:48:02 +0500

    работа над кнопкой списка

---
+++ dropdown.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById('btn');
+      
+        btn.addEventListener('click', function(e) {
+          let list = document.getElementById('list');
+      
+          if (list.style.display === 'none') {
+            list.style.display = 'block';
+          } else {
+            list.style.display = 'none';
+          }
+      
+        });  
