394a617ef9bf39c09b506c56c5abeb61332939a6
Kalega1 <kalegin.m@yandex.ru>
2023-02-23 12:55:22 +0500

    мучаюсь с кнопкой логаут

--- js/users.js
+++ js/users.js
@@ -1,8 +1,5 @@
-auth.onAuthStateChanged(user => {
-    if(user) {
-        console.log('user is signed in at users.html');
-    } else {
-        alert('you login session has expired or you have logged out to cont')
-        location = "index.html"
-    }
-})
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
